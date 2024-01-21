e77c545acbe94634daad57b0e04bd29b31a4398f
Caitlin-Wilson-8642 <wils1353@purdue.edu>
2024-01-20 21:49:31 -0500

    Filters Applied in main_page.js

--- main_page.js
+++ main_page.js
@@ -14,16 +14,22 @@ function onLoad() {
 
     //myGeneralCache.internships[:].info
     myFilteredCache = myGeneralCache;
+    print_postings();
 }
 
 /*
 Filters internships by location, major, and city
  */
+
+function make_filters() {
+
+}
 function filterNow() {
     onLoad();
     filter_major();
     filter_city();
     filter_skills();
+    filter_salary();
 }
 
 function getSelectedMajors() {
@@ -101,6 +107,69 @@ function filter_skills() {
         myFilteredCache = ft.skillSearch(myFilteredCache, getSelectedSkills());
     }
     return myFilteredCache;
+}
+
+function get_min_salary () {
+    let x = document.getElementById("salary_select").value;
+    try {
+        var y = parseFloat(x);
+        if (isNaN(y)) {
+            throw new Error();
+        }
+    } catch (error) {
+        y = 0;
+    }
+    return y;
+}
+function filter_salary() {
+    var minSalary = get_min_salary();
+    myFilteredCache = ft.inSalaryRange(myFilteredCache, minSalary, 0);
+}
+
+function print_postings() {
+    var internshipPostings = document.getElementbyId("internship_postings");
+    myFilteredCache.forEach(function(item) {
+        var divElement = document.createElement("div");
+        divElement.className = "internship_post";
+        divElement.id = "post_outer_box"
+
+        var headElement = document.createElement("h2");
+        headElement.textContent = item.company + ": " + item.position;
+        headElement.id = "job_header";
+
+        var job_info_div = document.createElement("div");
+        job_info_div.id = "job_info_left_div";
+        job_info_div.className = "job_info"
+
+        var paragraph_info = document.createElement("p");
+        paragraph_info.textContent = "Salary: " + item.salary + "\n"
+            + "Location: " + item.location + "\n"
+            + "Skills: " + item.skills + "\n"
+            + "Degree: " + item.degree;
+
+        job_info_div.appendChild(paragraph_info);
+
+        var job_logo_div = document.createElement("div");
+        job_logo_div.id = "job_logo_div";
+        job_logo_div.className = "job_logo_div";
+
+        var item_logo = document.createElement("img");
+        item_logo.src = item.logo;
+
+        var learn_button = document.createElement("button");
+        learn_button.textContent = "Learn More"
+        learn_button.onclick = alert("This is going to open a link when we fix it lol");
+
+        job_logo_div.appendChild(job_logo_div);
+        job_logo_div.appendChild(learn_button);
+
+        divElement.appendChild(headElement);
+        divElement.appendChild(job_info_div);
+        divElement.appendChild(job_logo_div);
+
+        //divElement.innerHTML = "\"<strong>Name:</strong> \" + item.name + \", <strong>Info:</strong> \" + item.company;"
+        internshipPostings.appendChild(divElement);
+    })
 }
 
 
