ddffc479f2851b02ace89cb47fca0b69b954b4ba
ChrisPrice03 <112359083+ChrisPrice03@users.noreply.github.com>
2024-01-21 04:49:38 -0500

    Update test.js

--- test.js
+++ test.js
@@ -1,6 +1,5 @@
-window.onload = function () {
-        alert("loaded")
-}
+import * as job_funcs from './internship.js'
+import {FilterTool, Internship, InternshipCache, InternshipLoader} from "./internship.js";
 
 function alertMe() {
   window.alert("Button clicked!");
